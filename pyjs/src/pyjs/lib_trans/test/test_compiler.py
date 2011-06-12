1a6d3737bd0d25e70a7d8ae6a50d623d84e003c2
Daniel Kluev <dan.kluev@gmail.com>
2011-06-12 21:00:43 +1100

    Some tests for compiler

--- pyjs/src/pyjs/lib_trans/test/test_compiler.py
+++ pyjs/src/pyjs/lib_trans/test/test_compiler.py
@@ -1,23 +1,67 @@
+from UnitTest import UnitTest
+from textwrap import dedent
+import os.path
 import sys
-sys.path[0:0] = ['/root/repos/pyjamas/pyjs/src/pyjs/lib_trans/']
+sys.path[0:0] = [os.path.dirname(os.path.dirname(os.path.abspath(__file__)))]
 import compiler
 
-src = """
-a = 1
+class CompilerTest(UnitTest):
+    def _test_compile(self, code, codestr):
+        code2 = dedent(code) + "\n"
+        tree = compiler.parse(code2)
+        self.assertEqual(str(tree.node), codestr)
 
-def test():
-    pass
+    def test_statements(self):
+        statements = [
+            ("a + 1",
+             "Stmt([Discard(Add(Name('a'), Const(1)))])"),
+            
+            ("a = 1",
+             "Stmt([Assign([AssName('a', 'OP_ASSIGN')], Const(1))])"),
+            
+            ("def test(): pass",
+             "Stmt([Function(None, 'test', (), (), False,"
+             " False, None, Stmt([Pass()]))])"),
+            
+            ("with a as b: pass",
+             "Stmt([With(Name('a'), AssName('b', 'OP_ASSIGN'),"
+             " Stmt([Pass()]))])"),
+            
+            ("""
+            def test(a, b=123, **kw):
+                yield b
+            """,
+               "Stmt([Function(None, 'test', ['a', 'b', 'kw'],"
+               " [Const(123)], False, True, None,"
+               " Stmt([Discard(Yield(Name('b')))]))])"),
+            
+            ("""
+            @dec
+            class X(object):
+                pass
+            """,
+               "Stmt([Class('X', [Name('object')], None,"
+               " Stmt([Pass()]), Decorators([Name('dec')]))])"),
+            
+            ("""
+            @dec()
+            def test():
+                pass
+            """,
+               "Stmt([Function(Decorators([CallFunc(Name('dec'), [], None, None)]),"
+               " 'test', (), (), False, False, None, Stmt([Pass()]))])"),
+        
+        ]
+            
+        for code, codestr in statements:
+            self._test_compile(code, codestr)
 
-with a as b:
-    pass
+from RunTests import RunTests
+def test_main():
+    t = RunTests()
+    t.add(CompilerTest)
+    t.start_test()
 
-@test
-class X(object):
-    pass
+if __name__ == '__main__':
+    test_main()
 
-@test()
-def test2():
-    pass
-"""
-tree = compiler.parse(src)
-print tree
