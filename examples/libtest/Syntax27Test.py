015a26d82e8be5346e2a291251497afd1ea53e0c
Daniel Kluev <dan.kluev@gmail.com>
2011-07-01 01:51:38 +1100

    Dict comprehensions tests

--- examples/libtest/Syntax27Test.py
+++ examples/libtest/Syntax27Test.py
@@ -62,3 +62,25 @@ class Syntax27Test(UnitTest):
             return {x() for x in items}
         self.assertEqual(test_func(), set(range(5)))
                     
+    def testDictComprehensions(self):
+        k = "old value"
+        d = { k: None for k in range(10) }
+        self.assertEqual(d, {0: None, 1: None, 2: None, 3: None, 4: None, 5: None, 6: None, 7: None, 8: None, 9: None})
+        self.assertEqual(k, 'old value')
+        
+        d = { k: k+10 for k in range(10) }
+        self.assertEqual(d, {0: 10, 1: 11, 2: 12, 3: 13, 4: 14, 5: 15, 6: 16, 7: 17, 8: 18, 9: 19})
+        
+        d = { k: v for k in range(10) for v in range(10) if k == v }
+        self.assertEqual(d, {0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9})
+        
+        d = { k: v for v in range(10) for k in range(v*9, v*10) }
+        self.assertEqual(d, {9: 1, 18: 2, 19: 2, 27: 3, 28: 3, 29: 3, 36: 4, 37: 4, 38: 4, 39: 4, 45: 5,
+            46: 5, 47: 5, 48: 5, 49: 5, 54: 6, 55: 6, 56: 6, 57: 6, 58: 6, 59: 6, 63: 7, 64: 7,
+            65: 7, 66: 7, 67: 7, 68: 7, 69: 7, 72: 8, 73: 8, 74: 8, 75: 8, 76: 8, 77: 8, 78: 8,
+            79: 8, 81: 9, 82: 9, 83: 9, 84: 9, 85: 9, 86: 9, 87: 9, 88: 9, 89: 9})
+        
+        g = "outer"
+        d = { k: g for k in range(3) }
+        self.assertEqual(d, {0: 'outer', 1: 'outer', 2: 'outer'})
+        
