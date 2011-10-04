6a447449fa9acd54fcade3ffad54000f8d4e56b2
Luke Leighton <lkcl@lkcl.net>
2011-10-04 13:19:11 +0100

    crossword example, AGPLv3 licensed

---
+++ examples/crossword/Crossword.py
@@ -0,0 +1,340 @@
+# Crossword Puzzle Loader Demo
+# Copyright (C) 2011 Camille Dalmeras
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import pyjd
+
+from pyjamas.ui.RootPanel import RootPanel
+from pyjamas.ui.HTML import HTML
+from pyjamas.ui.Label import Label
+from pyjamas.ui.Button import Button
+from pyjamas.ui.HorizontalPanel import HorizontalPanel
+from pyjamas.ui.AbsolutePanel import AbsolutePanel
+from pyjamas.ui.ScrollPanel import ScrollPanel
+from pyjamas.ui.Grid import Grid
+from pyjamas.ui.TextBox import TextBox
+from pyjamas.ui.FocusPanel import FocusPanel
+from pyjamas.ui.Image import Image
+from pyjamas.ui.DockPanel import DockPanel
+from pyjamas.ui import HasHorizontalAlignment
+from pyjamas.ui import HasVerticalAlignment
+from pyjamas.ui import HasAlignment
+from pyjamas.ui import Event
+from pyjamas.ui import KeyboardListener
+from pyjamas.ui.PopupPanel import PopupPanel
+from pyjamas import Window
+from pyjamas import DOM
+
+#from pyjamas.HorizSplitPanel import HorizontalSplitPanel
+
+from pyjamas.JSONService import JSONProxy
+
+from pyjamas.Timer import Timer
+
+def copy(ind):
+    res = {}
+    for (k, v) in ind.items():
+        r1 = {}
+        if k.isdigit(): # argh!  bug in pyjd json handling! int turns to string
+            k = int(k) 
+        if isinstance(v, dict):
+            v = copy(v)
+        res[k] = v
+    return res
+
+
+class ClueDialog(PopupPanel):
+    def __init__(self, xword):
+        PopupPanel.__init__(self, True)
+
+        clue = xword.find_clue()
+        print clue
+        contents = HTML(clue)
+        contents.setWidth("200px")
+        self.setWidget(contents)
+        row = xword.word_selected_pos[0]
+        col = xword.word_selected_pos[1]
+        cell = xword.tp.getWidget(row, col)
+
+        self.setStyleName("clue-popup")
+        left = cell.getAbsoluteLeft() + 40
+        top = cell.getAbsoluteTop() + 40
+        self.setPopupPosition(left, top)
+        self.show()
+
+
+class Crossword(FocusPanel):
+
+    def __init__(self):
+
+        #AbsolutePanel.__init__(self, Size=("100%", "100%"))
+        FocusPanel.__init__(self)
+
+        self.remote = InfoServicePython()
+
+        # grid for crossword
+        self.tp = Grid(StyleName='crossword',
+                       CellSpacing="0px", CellPadding="0px",
+                       zIndex=0)
+        self.add(self.tp)
+        self.cf = self.tp.getCellFormatter()
+        self.cd = None
+
+        width = Window.getClientWidth()
+        height = Window.getClientHeight()
+
+        self.onWindowResized(width, height)
+        Window.addWindowResizeListener(self)
+  
+        self.word_selected = None
+        self.word_selected_pos = None
+
+        self.tp.addTableListener(self)
+        self.addKeyboardListener(self)
+        self.setFocus(True)
+
+    def onKeyDown(self, sender, keycode, modifiers):
+
+        # check up/down/left/right cursor keys
+        if keycode == KeyboardListener.KEY_UP or \
+           keycode == KeyboardListener.KEY_LEFT:
+            self.move_cursor(-1)
+            return
+        elif keycode == KeyboardListener.KEY_DOWN or \
+           keycode == KeyboardListener.KEY_RIGHT:
+            self.move_cursor(1)
+            return
+        
+        # check the key is a letter, and there's a grid position highlighted
+        val = chr(keycode)
+        if not val.isalpha() or not self.word_selected_pos:
+            return
+
+        # update value
+        row = self.word_selected_pos[0]
+        col = self.word_selected_pos[1]
+        self.set_grid_value(val, row, col)
+
+        # move cursor onwards (if possible)
+        self.move_cursor(1)
+
+    def move_cursor(self, dirn):
+
+        word = self.words[self.word_selected]
+        x1 = word['x']
+        y1 = word['y']
+        xd = word['xd']
+        yd = word['yd']
+        x2 = x1 + xd
+        y2 = y1 + yd
+
+        row = self.word_selected_pos[0]
+        col = self.word_selected_pos[1]
+
+        if dirn == 1 and x2 == col+1 and y2 == row+1:
+            return
+
+        if dirn == -1 and x1 == col+1 and y1 == row+1:
+            return
+
+        self.highlight_cursor(False)
+        self.word_selected_pos = (row + (yd and dirn), col + (xd and dirn))
+        self.highlight_cursor(True)
+        #self.highlight_input(True)
+
+    def onKeyUp(self, sender, keycode, modifiers):
+        pass
+
+    def onKeyPress(self, sender, keycode, modifiers):
+        pass
+
+    def highlight_cursor(self, highlight):
+        """ highlights (or dehighlights) the currently selected cell
+        """
+        row = self.word_selected_pos[0]
+        col = self.word_selected_pos[1]
+        self.cf._setStyleName(row, col, "cross-square-word-cursor",
+                                      highlight)
+
+    def highlight_selected(self, highlight):
+        """ highlights (or dehighlights) the currently-selected word
+        """
+        word = self.words[self.word_selected]
+        x1 = word['x']
+        y1 = word['y']
+        x2 = x1 + word['xd']
+        y2 = y1 + word['yd']
+        for x in range(x1, x2+1):
+            for y in range(y1, y2+1):
+                self.cf._setStyleName(y-1, x-1, "cross-square-word-select",
+                                      highlight)
+
+    def _find_clue(self, clues):
+        num = self.word_selected
+        print num, clues
+        if not clues.has_key(num):
+            return None
+        clue = clues[num]
+        return "%s (%d)" % (clue['word'], clue['format'])
+
+    def find_clue(self):
+        return self._find_clue(self.across['clues']) or \
+               self._find_clue(self.down['clues'])
+
+    def check_word_range(self, word, row, col):
+        """ checks if a word is "hit" by the row/col selected
+        """
+        x1 = word['x']
+        y1 = word['y']
+        x2 = x1 + word['xd']
+        y2 = y1 + word['yd']
+        for x in range(x1, x2+1):
+            for y in range(y1, y2+1):
+                if x == col and y == row:
+                    return True
+        return False
+            
+    def find_word(self, row, col):
+        found_words = []
+        for (num, word) in self.words.items():
+            if self.check_word_range(word, row, col):
+                found_words.append(num)
+        return found_words
+            
+    def onCellClicked(self, listener, row, col):
+
+        # find the words first
+        words_found = self.find_word(row+1, col+1)
+
+        # de-highlight the word found
+        if self.word_selected is not None:
+            self.highlight_selected(False)
+
+        # de-highlight cursor
+        if self.word_selected_pos is not None:
+            self.highlight_cursor(False)
+
+        if not words_found:
+            self.word_selected_pos = None
+            return
+
+        new_word = words_found[0] # pick any one
+
+        # work out which one to highlight
+        if self.word_selected in words_found:
+            # see if the same letter was clicked, first
+            if self.word_selected_pos is not None and \
+               (self.word_selected_pos[0] != row or \
+                self.word_selected_pos[1] != col):
+                # it wasn't (the same letter): keep the same word
+                new_word = self.word_selected
+            else:
+                # whoops, same letter clicked: pick the other alternative
+                for w in words_found:
+                    if w != self.word_selected:
+                        new_word = w
+                        break
+
+        # store current word and cursor pos, highlight word and pos, show clue
+        self.word_selected = new_word
+        self.word_selected_pos = (row, col)
+        self.highlight_selected(True)
+        self.highlight_cursor(True)
+
+        # show the clue popup
+        if self.cd is not None:
+            self.cd.hide()
+            del self.cd
+        self.cd = ClueDialog(self)
+
+    def onWindowResized(self, width, height):
+        self.remote.get_crossword(self)
+        return
+        #self.hp.setWidth("%dpx" % (width - self.tree_width))
+        #self.hp.setHeight("%dpx" % (height - 20))
+        self.cp1.setHeight("%dpx" % (height - 30))
+        self.cp2.setHeight("%dpx" % (height - 30))
+        self.rps.setHeight("%dpx" % (height - 30))
+        self.horzpanel1.setHeight("%dpx" % (height - 20))
+
+    def set_grid_value(self, clue, y, x):
+
+        style = clue and "cross-square" or "cross-square-block"
+        clue = clue or '&nbsp;'
+        self.tp.setWidget(y, x, HTML(clue, StyleName=style))
+        self.cf.setAlignment(y, x,  HasAlignment.ALIGN_CENTER,
+                                    HasAlignment.ALIGN_MIDDLE)
+
+    def create_crossword(self, cross):
+        """ creates the crossword, records all info from the server
+        """
+
+        # take a copy of everything
+        self.words = copy(cross['words'])
+        self.across = copy(cross['across'])
+        self.down = copy(cross['down'])
+
+        # set up the letters (demo)
+        self.letters = []
+        cells = cross["cells"]
+        l = len(cells)
+        self.tp.resize(cross['height'], cross['width'])
+        for c in cells:
+            x = c['x'] - 1
+            y = c['y'] - 1
+            clue = c['value']
+            self.letters.append({'x': x, 'y': y, 'value': clue})
+            self.set_grid_value(clue and "&nbsp;" or None, y, x)
+
+    def fill_crossword(self):
+
+        # set up the letters (demo)
+        for c in self.letters:
+            x = c['x']
+            y = c['y']
+            clue = c['value']
+            self.set_grid_value(clue, y, x)
+
+    def fill_right_grid(self, data):
+        index = data.get('index')
+        name = data.get('name')
+        if data.has_key('items'):
+            self.rp.add_items(data.get('items'), name, index)
+        elif data.has_key('html'):
+            self.rp.add_html(data.get('html'), name, index)
+
+    def onRemoteResponse(self, response, request_info):
+        method = request_info.method
+        if method == "get_crossword":
+            self.create_crossword(response)
+
+    def onRemoteError(self, code, message, request_info):
+        RootPanel().add(HTML("Server Error" + str(code)))
+        RootPanel().add(HTML(str(message)))
+
+class InfoServicePython(JSONProxy):
+    def __init__(self):
+            JSONProxy.__init__(self, "/crosswordservice/CrosswordService.py",
+                    ["get_crossword",
+                     ])
+
+
+
+if __name__ == '__main__':
+    pyjd.setup("http://127.0.0.1/examples/crossword/public/Crossword.html")
+    app = Crossword()
+    RootPanel("crossword").add(app)
+    pyjd.run()
+
---
+++ examples/crossword/public/services/CrosswordService.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+
+import os
+from string import split, strip
+import xml.dom.minidom
+
+def get_directory_info(prefix, pth, recursive):
+    res = []
+    directory = os.listdir(pth)
+    directory.sort()
+    for p in directory:
+        if p[0] != '.':
+            subp = os.path.join(pth, p)
+            p = os.path.join(prefix, p)
+            if recursive and os.path.isdir(subp):
+                res.append([p, get_directory_info(prefix, subp, 1)])
+            else:
+                res.append([p, None])
+    return res
+
+def get_cells(dom):
+    cells = []
+    for c in dom:
+        x = int(c.attributes['x'].nodeValue)
+        y = int(c.attributes['y'].nodeValue)
+        v = None
+        if c.hasAttribute('solution'):
+            v = c.attributes['solution'].nodeValue
+        cells.append({'x': x, 'y': y, 'value': v})
+    return cells
+
+def get_words(dom):
+    words = {}
+    for c in dom:
+        x = c.attributes['x'].nodeValue
+        y = c.attributes['y'].nodeValue
+        if '-' in x:
+            y = int(y)
+            yd = 0
+            x = x.split("-")
+            x = map(int, x)
+            xd = x[1] - x[0]
+            x = x[0]
+        else:
+            x = int(x)
+            xd = 0
+            y = y.split("-")
+            y = map(int, y)
+            yd = y[1] - y[0]
+            y = y[0]
+        num = int(c.attributes['id'].nodeValue)
+        word = { 'x': x, 'y': y, 'xd': xd, 'yd': yd, 'id': num,
+               }
+        words[num] = word
+    return words
+
+def get_clues(dom):
+    title = dom.getElementsByTagName("title")[0]
+    title = "".join(map(lambda x: x.toxml(), title.childNodes))
+    dclues = dom.getElementsByTagName("clue")
+    clues = {}
+    for c in dclues:
+        num = int(c.attributes['word'].nodeValue)
+        clue = { 'word': unicode(c.childNodes[0].data),
+                 'format': int(c.attributes['format'].nodeValue),
+               }
+        clues[num] = clue
+    return {'title': title, 'clues': clues}
+
+class Service:
+    def get_crossword(self):
+        """ return list of directory, including indicating whether each
+            path is a directory or not
+        """
+        pth = os.path.join(os.getcwd(), "data")
+        pth = os.path.join(pth, "test.xml")
+        f = open(pth)
+        txt = f.read()
+        dom = xml.dom.minidom.parseString(txt)
+        dclues = dom.getElementsByTagName("clues")
+        across = get_clues(dclues[0])
+        down = get_clues(dclues[1])
+        words = get_words(dom.getElementsByTagName("word"))
+        cells = get_cells(dom.getElementsByTagName("cell"))
+        width = 9
+        height = 9
+        res = {'across': across, 'down': down,
+               'width': width, 'height': height,
+               'cells': cells, 'words': words,
+               }
+        return res
+
+from jsonrpc.cgihandler import handleCGIRequest
+
+handleCGIRequest(Service())
+
+def test():
+    s = Service()
+    print s.get_crossword()
+
+#if __name__ == '__main__':
+#    test()
