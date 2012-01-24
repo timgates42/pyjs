7f7d865ce0addfecc428d9da0c3021a8492f3b1a
maho <maho@dlaptop.local>
2012-01-24 12:04:46 +0100

    - added copyright notes

--- library/gwt/ui/Calendar.py
+++ library/gwt/ui/Calendar.py
@@ -1,6 +1,7 @@
 ﻿# Date Time Example
 # Copyright (C) 2009 Yit Choong (http://code.google.com/u/yitchoong/)
 # Copyright (C) 2009 Luke Kenneth Casson Leighton <lkcl@lkcl.net>
+# Copyright (C) 2012 Łukasz Mach <maho@pagema.net>
 
 from pyjamas.ui.SimplePanel import SimplePanel
 from pyjamas import Factory
