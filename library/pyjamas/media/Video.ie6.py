0e8e0742311c9fe2ca97c5b46e96c36898ca4626
Luke Leighton <lkcl@lkcl.net>
2012-03-30 23:00:00 +0100

    try different stuff with ie6

--- library/pyjamas/media/Video.ie6.py
+++ library/pyjamas/media/Video.ie6.py
@@ -4,10 +4,11 @@ class Video(Media):
     def __init__(self, src=None, **kwargs):
         print "create object"
         obj = DOM.createElement("OBJECT")
-        #DOM.setAttribute(obj, "TYPE", "application/x-mplayer2")
-        DOM.setAttribute(obj, "type", "application/x-oleobject")
-        #DOM.setAttribute(obj, "classid",
-        #                        "CLSID:22D6F312-B0F6-11D0-94AB-0080C74C7E95")
+        DOM.setAttribute(obj, "TYPE", "application/x-mplayer2")
+        #DOM.setAttribute(obj, "type", "application/x-oleobject")
+        DOM.setAttribute(obj, "classid",
+                                #"CLSID:22D6F312-B0F6-11D0-94AB-0080C74C7E95")
+                                "CLSID:6BF52A52-394A-11d3-B153-00C04F79FAA6")
         print "set element"
         self.setElement(obj)
 
@@ -18,7 +19,7 @@ class Video(Media):
         if src:
             self.setSrc(src)
 
-        self.setID("MediaPlayer")
+        #self.setID("MediaPlayer")
 
         self.dispparam = DOM.createElement("PARAM")
         DOM.setAttribute(self.dispparam, "name", "ShowDisplay")
@@ -27,10 +28,12 @@ class Video(Media):
 
     def setSrc(self, src):
         print "setSrc", src
-        self.srcparam = DOM.createElement("PARAM")
-        DOM.setAttribute(self.srcparam, "name", "FileName")
-        DOM.setAttribute(self.srcparam, "VALUE", src)
-        self.getElement().appendChild(self.srcparam)
+        #self.srcparam = DOM.createElement("PARAM")
+        #DOM.setAttribute(self.srcparam, "name", "FileName")
+        #DOM.setAttribute(self.srcparam, "VALUE", src)
+        #self.getElement().appendChild(self.srcparam)
+        obj = self.getElement()
+        obj.URL = src
 
     def setControls(self, controls):
         print "setControls", controls
