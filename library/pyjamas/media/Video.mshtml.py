94d2e04ea373b214f8ce9341dacd46384b065167
Luke Leighton <lkcl@lkcl.net>
2012-03-30 22:38:00 +0100

    try different stuff with ie6

--- library/pyjamas/media/Video.mshtml.py
+++ library/pyjamas/media/Video.mshtml.py
@@ -3,29 +3,13 @@ class Video(Media):
 
     def __init__(self, src=None, **kwargs):
         print "create object"
-        obj = DOM.createElement("object")
+        obj = DOM.createElement("OBJECT")
         DOM.setAttribute(obj, "type", "application/x-mplayer2")
-        #obj.pluginspage = \
-        #   "http://www.microsoft.com/windows/windowsmedia/default.aspx"
+        #DOM.setAttribute(obj, "type", "application/x-oleobject")
+        #DOM.setAttribute(obj, "classid",
+        #                        "CLSID:22D6F312-B0F6-11D0-94AB-0080C74C7E95")
         print "set element"
         self.setElement(obj)
-
-        self.srcparam = DOM.createElement("param")
-        DOM.setAttribute(self.srcparam, "name", "src")
-        self.ctrlparam = DOM.createElement("param")
-        DOM.setAttribute(self.ctrlparam, "name", "ShowControls")
-        self.statparam = DOM.createElement("param")
-        DOM.setAttribute(self.statparam, "name", "ShowStatusBar")
-        self.loopparam = DOM.createElement("param")
-        DOM.setAttribute(self.loopparam, "name", "autorewind")
-        self.playparam = DOM.createElement("param")
-        DOM.setAttribute(self.playparam, "name", "autostart")
-
-        obj.appendChild(self.srcparam)
-        obj.appendChild(self.statparam)
-        obj.appendChild(self.loopparam)
-        obj.appendChild(self.ctrlparam)
-        obj.appendChild(self.playparam)
 
         print "widget init"
         Media.__init__(self, **kwargs)
@@ -36,26 +20,40 @@ class Video(Media):
 
     def setSrc(self, src):
         print "setSrc", src
-        DOM.setAttribute(self.srcparam, "value", src)
-
-    def setWidth(self, width):
-        print "setWidth", width
-        DOM.setAttribute(self.element, "width", width)
-
-    def setHeight(self, height):
-        print "setHeight", height
-        DOM.setAttribute(self.element, "height", height)
+        self.srcparam = DOM.createElement("PARAM")
+        DOM.setAttribute(self.srcparam, "name", "FileName")
+        DOM.setAttribute(self.srcparam, "VALUE", src)
+        obj.appendChild(self.srcparam)
 
     def setControls(self, controls):
         print "setControls", controls
-        DOM.setBooleanAttribute(self.ctrlparam, "value", controls)
+        self.ctrlparam = DOM.createElement("PARAM")
+        DOM.setAttribute(self.ctrlparam, "name", "ShowControls")
+        DOM.setBooleanAttribute(self.ctrlparam, "VALUE",
+            controls and "true" or "false")
+        obj.appendChild(self.ctrlparam)
 
     def setStatusbar(self, statusbar):
-        DOM.setBooleanAttribute(self.statparam, "value", statusbar)
+        print "setstatus", statusbar
+        self.statparam = DOM.createElement("PARAM")
+        DOM.setAttribute(self.statparam, "name", "ShowStatusBar")
+        DOM.setBooleanAttribute(self.statparam, "VALUE",
+            statusbar and "true" or "false")
+        obj.appendChild(self.statparam)
 
     def setLoop(self, autorewind):
-        DOM.setBooleanAttribute(self.loopparam, "value", autorewind)
+        print "autorewind", autorewind
+        self.loopparam = DOM.createElement("PARAM")
+        DOM.setAttribute(self.loopparam, "name", "autorewind")
+        DOM.setBooleanAttribute(self.loopparam, "VALUE", 
+            autorewind and "true" or "false")
+        obj.appendChild(self.loopparam)
 
     def setAutoplay(self, autostart):
-        DOM.setBooleanAttribute(self.playparam, "value", autostart)
+        print "autoplay", autostart
+        self.playparam = DOM.createElement("PARAM")
+        DOM.setAttribute(self.playparam, "name", "autostart")
+        DOM.setBooleanAttribute(self.playparam, "VALUE", 
+            autostart and "true" or "false")
+        obj.appendChild(self.playparam)
 
