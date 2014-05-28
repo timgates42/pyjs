d37f91f50dd6c0c3202258daca95ee6ee111688f
Janjaap Bos <j.bos@capitar.com>
2014-05-28 23:22:42 +0200

    Fix for IE 11 (Focus)
    IE presents itself as mozilla, so it trips on the bug.

--- pyjswidgets/pyjamas/ui/Focus.oldmoz.py
+++ pyjswidgets/pyjamas/ui/Focus.oldmoz.py
@@ -43,6 +43,4 @@ def createFocusable0(focusHandler):
     """)
 
 def createFocusable():
-    ensureFocusHandler()
-    return createFocusable0()
-
+    return createFocusable0(ensureFocusHandler())
